963f29bbd1b9893da2c2769ff8efc5cdfc965051
Gabriela <gabrielamalasquez126@gmail.com>
2024-06-15 14:58:35 +0200

    Ejercicio terminado

--- library/timer/TimerPartComponent.js
+++ library/timer/TimerPartComponent.js
@@ -1,36 +1,61 @@
-import { LitElement, html, css } from 'lit';
-
-export class TimerPartComponent extends LitElement {
-    static properties = {
-        value: { type: Number, attribute: true },
-        format: { type: String, attribute: true }
-    }
-
+export class TimerPartComponent extends HTMLElement {
     constructor() {
         super();
-    }
-
-    static styles = css`
-        :host {
-            color: var(--timer-component-part-color);
-            background-color: var(--timer-component-part-background-color);
-            padding: var(--timer-component-part-padding);
-            box-shadow: var(--timer-component-part-box-shadow);
-            border-radius: var(--timer-component-part-border-radius);
-        }
-    `;
-
-    render() {
-        return html`
-            <div>
-                ${this._formatTime(this.value)}
+        this.attachShadow({ mode: 'open' }).innerHTML = `
+            <style>
+                .time-part {
+                    color: var(--neutral-color);
+                    padding: 10px;
+                    box-shadow: 0 2px 3px 0 rgba(0,0,0,0.2);
+                    border-radius: 5px;
+                    margin: 10px;
+                }
+                .tiempo {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    margin-bottom: 20px;
+                }
+            </style>
+            <div class="tiempo">
+                <span class="time-part"></span>
             </div>
         `;
+        this.segundos = Number(this.getAttribute('start'));
+        this.interval = null;
+        this.updateTime();
     }
 
-    _formatTime(time) {
-        if (this.format?.length > 1) return time < 10 ? `0${time}` : time;
-        return time;
+    startTime() {
+        if (!this.interval) {
+            this.interval = setInterval(() => {
+                this.segundos--;
+                if (this.segundos <= 0) {
+                    clearInterval(this.interval);
+                    this.dispatchEvent(new CustomEvent('endTime', { bubbles: true })); 
+                }
+                this.updateTime();
+            }, 1000);
+        }
+    }
+
+    pauseTime() {
+        clearInterval(this.interval);
+        this.interval = null;
+    }
+
+    resetTime() {
+        this.pauseTime();
+        this.segundos = Number(this.getAttribute('start'));
+        this.updateTime();
+    }
+
+    updateTime() {
+        const horas = ('0' + Math.floor(this.segundos / 3600)).slice(-2);
+        const minutos = ('0' + Math.floor((this.segundos % 3600) / 60)).slice(-2);
+        const segundos = ('0' + (this.segundos % 60)).slice(-2);
+        this.shadowRoot.querySelector('.time-part').textContent = `${horas} : ${minutos} : ${segundos}`;
     }
 }
-customElements.define('time-part-component', TimerPartComponent);
+
+customElements.define('timer-part-component', TimerPartComponent);
